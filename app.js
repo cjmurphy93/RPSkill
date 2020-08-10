489d0ca6fcfc2a52199563b735b13f6ee56c61ef
cjmurphy93 <cjmurph93@gmail.com>
2020-08-10 11:09:57 -0400

    cleaning for database

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const mongoose = require("mongoose");
 app.get("/", (req, res) => res.send("Hello World"));
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
