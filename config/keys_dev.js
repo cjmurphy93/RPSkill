4779f100cac69628d6d724b606a33b239e99dde2
Austin Wong <61334738+CallMeAustino@users.noreply.github.com>
2020-08-12 10:19:52 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dev:PMGsbMa6h9MRsAb@cluster0.efgio.mongodb.net/MERNdb?retryWrites=true&w=majority",
-    secretOrKey: "d3uhH2}ax?O{V/fNX7.wF`gsG{;):$"
-};
