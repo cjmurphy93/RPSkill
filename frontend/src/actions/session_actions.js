120464b5ee4f1d372018a25fbdc8770c901591d5
Austin <austin.hokun.wong@gmail.com>
2020-08-12 11:59:56 -0400

    added action and leaderboardapiutil

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,11 +1,16 @@
 import jwt_decode from 'jwt-decode';
 import * as APIUtil from "../util/session_api_util";
+import * as LeaderboardUtil from "../util/leaderboard_api_util";
+
 
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const REMOVE_ERRORS = "REMOVE_ERRORS";
+export const RECEIVE_USERS = "RECEIVE_USERS";
+
+
 
 export const receiveCurrentUser = (currentUser) => ({
     type: RECEIVE_CURRENT_USER,
@@ -27,6 +32,10 @@ export const receiveErrors = (errors) => ({
 
 export const removeErrors = () => ({
         type: REMOVE_ERRORS,
+});
+
+export const removeErrors = () => ({
+    type: RECEIVE_USERS,
 });
 
 export const signup = (user) => dispatch => (
@@ -57,3 +66,12 @@ export const logout = () => dispatch => {
 export const remove = () => dispatch => {
     dispatch(removeErrors())
 }
+
+export const scores = () => dispatch => (
+    LeaderboardUtil.users()
+        .then((res) => {
+            dispatch(receiveUsers(res));
+        }).catch((err) => {
+            dispatch(receiveErrors(err.response.data));
+        })
+);
