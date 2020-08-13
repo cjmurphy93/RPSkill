349637e9bc96fc7cf32b5858e167aa64199d0d5f
cjmurphy93 <cjmurph93@gmail.com>
2020-08-13 14:56:58 -0400

    started backend sockets

--- app.js
+++ app.js
@@ -5,10 +5,39 @@ const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
+const server = require('http').createServer(app);
+const socketio = require("socket.io");
+const io = socketio(server);
+const {addPlayer, removePlayer, getPlayer, getPlayersInGame} = require("./gameManager");
 
 const users = require("./routes/api/users");
 const leaderboard = require("./routes/api/leaderboard");
 const games = require("./routes/api/games");
+
+io.on("connect", (socket) => {
+    socket.on("join", ({username, game}, callback) => {
+        const { player, error } = addPlayer({ id: socket.id, username, game });
+        if  (error) return callback(error);
+        socket.join(player.game);
+
+        socket.emit("id", socket.id);
+
+        io.to(player.game).emit("gameData", {
+            game: player.game,
+            players: getPlayersInGame(player.game)
+        });
+        callback();
+    });
+    socket.on("disconnect", () => {
+        const player = removePlayer(socket.id);
+        if (player){
+            io.to(player.game).emit("gameData", {
+              game: player.game,
+              players: getPlayersInGame(player.game),
+            });
+        }
+    });
+});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"));
@@ -16,6 +45,7 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
     });
 }
+
 
 mongoose
 .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
