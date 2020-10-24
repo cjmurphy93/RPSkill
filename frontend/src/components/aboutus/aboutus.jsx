1754582bf592d5861434835d68270ff23eb1f805
cjmurphy93 <cjmurph93@gmail.com>
2020-10-24 17:58:36 -0400

    changed my image

--- frontend/src/components/aboutus/aboutus.jsx
+++ frontend/src/components/aboutus/aboutus.jsx
@@ -11,15 +11,15 @@ class AboutUs extends React.Component {
                     <div className="about-container">
                         <div className="flex column center">
                             <h2 className="about-title">Nara Lee</h2>
-                            <a target="_blank"  rel="noreferrer" href="https://www.linkedin.com/in/naraskim/" className="user nara"> </a>
+                            <a target="_blank"  rel="noopener noreferrer" href="https://www.linkedin.com/in/naraskim/" className="user nara"> </a>
                         </div>
                         <div className="flex column center">
                             <h2 className="about-title">Connor Murphy</h2>
-                            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/connor-j-murphy/" className="user connor"> </a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/connor-j-murphy/" className="user connor"> </a>
                         </div>
                         <div className="flex column center">
                             <h2 className="about-title">Austin Wong</h2>
-                            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/austinhokunwong/" className="user austin"> </a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/austinhokunwong/" className="user austin"> </a>
                         </div>
                     </div>
                 </section>
